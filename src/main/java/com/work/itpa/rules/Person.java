214e751bfc8041d73371b2a00231a811b34b82b7
Ravindra Godbole <ravindra.godbole@cognizant.com>
2017-08-14 15:46:24 +0530

    Person  changed.

--- src/main/java/com/work/itpa/rules/Person.java
+++ src/main/java/com/work/itpa/rules/Person.java
@@ -264,6 +264,90 @@ public class Person implements Serializable {
 	public void setValid(boolean valid) {
 		this.valid = valid;
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((dateOfBirth == null) ? 0 : dateOfBirth.hashCode());
+		result = prime * result + disabilityPercent;
+		result = prime * result + ((disease == null) ? 0 : disease.hashCode());
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((relationShipCode == null) ? 0 : relationShipCode.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Person other = (Person) obj;
+		if (age != other.age) {
+			return false;
+		}
+		if (dateOfBirth == null) {
+			if (other.dateOfBirth != null) {
+				return false;
+			}
+		} else if (!dateOfBirth.equals(other.dateOfBirth)) {
+			return false;
+		}
+		if (disabilityPercent != other.disabilityPercent) {
+			return false;
+		}
+		if (disease == null) {
+			if (other.disease != null) {
+				return false;
+			}
+		} else if (!disease.equals(other.disease)) {
+			return false;
+		}
+		if (gender == null) {
+			if (other.gender != null) {
+				return false;
+			}
+		} else if (!gender.equals(other.gender)) {
+			return false;
+		}
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		if (relationShipCode == null) {
+			if (other.relationShipCode != null) {
+				return false;
+			}
+		} else if (!relationShipCode.equals(other.relationShipCode)) {
+			return false;
+		}
+		return true;
+	}
 	
 	
 	
