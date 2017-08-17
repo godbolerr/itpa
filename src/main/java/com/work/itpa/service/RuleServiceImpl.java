e27cf401d90b0414e83418333bd91bf2e3e2f896
Ravindra Godbole <ravindra.godbole@cognizant.com>
2017-08-17 09:45:19 +0530

    Deduction summary is calculated in the function instead of Rule

--- src/main/java/com/work/itpa/service/RuleServiceImpl.java
+++ src/main/java/com/work/itpa/service/RuleServiceImpl.java
@@ -4,18 +4,19 @@
 package com.work.itpa.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.kie.api.KieServices;
-import org.kie.api.event.rule.AfterMatchFiredEvent;
-import org.kie.api.event.rule.DebugRuleRuntimeEventListener;
-import org.kie.api.event.rule.DefaultAgendaEventListener;
 import org.kie.api.logger.KieRuntimeLogger;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.work.itpa.rules.Deduction;
 import com.work.itpa.rules.FinPerson;
 import com.work.itpa.rules.FinPersonResult;
 
@@ -34,24 +35,22 @@ public class RuleServiceImpl implements RuleService {
 
 	@Override
 	public FinPersonResult calculateBenefits(FinPerson person) {
-		
 
-    	KieSession kSession = kc.newKieSession("ItpaDataKs");
-    	KieRuntimeLogger logger =
-  			  KieServices.Factory.get().getLoggers().newFileLogger(kSession, "logRules");
-  	
-    	//kSession.addEventListener( new DebugRuleRuntimeEventListener() );
-    	
-//    	kSession.addEventListener( new DefaultAgendaEventListener() {
-//    	    public void afterMatchFired(AfterMatchFiredEvent event) {
-//    	        super.afterMatchFired( event );
-//    	        System.out.println( " ### " +  event );
-//    	    }
-//    	});
-//    	
-    	
-    	List<String> messages = new ArrayList<String>();
-    	
+		KieSession kSession = kc.newKieSession("ItpaDataKs");
+		KieRuntimeLogger logger = KieServices.Factory.get().getLoggers().newFileLogger(kSession, "logRules");
+
+		// kSession.addEventListener( new DebugRuleRuntimeEventListener() );
+
+		// kSession.addEventListener( new DefaultAgendaEventListener() {
+		// public void afterMatchFired(AfterMatchFiredEvent event) {
+		// super.afterMatchFired( event );
+		// System.out.println( " ### " + event );
+		// }
+		// });
+		//
+
+		List<String> messages = new ArrayList<String>();
+
 		FinPersonResult result = new FinPersonResult();
 
 		kSession.insert(person);
@@ -60,9 +59,39 @@ public class RuleServiceImpl implements RuleService {
 		kSession.fireAllRules();
 
 		logger.close();
-	    
-		
+
+		List<Deduction> plannedDeductions = calculateMaxPerCatetory(result.getDeductions());
+
+		result.setPlannedDeductions(plannedDeductions);
+
 		return result;
+	}
+
+	/**
+	 * Return planned deductions as list.
+	 * 
+	 * @param deductions
+	 * @return
+	 */
+	private List<Deduction> calculateMaxPerCatetory(List<Deduction> deductions) {
+
+		Map<String, Deduction> dMap = new HashMap<String, Deduction>();
+
+		for (Iterator<Deduction> iterator = deductions.iterator(); iterator.hasNext();) {
+			Deduction deduction = iterator.next();
+			String key = deduction.getSection() + deduction.getType();
+			if (dMap.containsKey(key) == false) {
+				dMap.put(key, deduction);
+			} else {
+				Deduction xDeduction = dMap.get(key);
+				if (xDeduction.getAmount().doubleValue() < deduction.getAmount().doubleValue()) {
+					dMap.put(key, deduction);
+				}
+			}
+		}
+
+		return new ArrayList<Deduction>(dMap.values());
+
 	}
 
 }
