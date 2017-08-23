186f918003f28118c154847b2653c977e2aa6a74
115750 <115750@192.168.0.5>
2017-08-23 09:17:57 +0530

    Tested first case.

--- src/main/java/com/work/itpa/itparules/ItparulesApplication.java
+++ src/main/java/com/work/itpa/itparules/ItparulesApplication.java
@@ -3,6 +3,12 @@ package com.work.itpa.itparules;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Drools based tax calculations
+ * 
+ * @author Developer
+ *
+ */
 @SpringBootApplication
 public class ItparulesApplication {
 
