5be03f9f6c589e2ca1299658711ba3955223cd90
115750 <115750@LT046399.cts.com>
2017-08-27 08:30:53 +0530

    80G Testing in progress

--- src/test/java/com/work/itpa/service/RuleTest.java
+++ src/test/java/com/work/itpa/service/RuleTest.java
@@ -265,7 +265,50 @@ public class RuleTest {
 		
 	}	
 	
+	@Test
+	public void test80GResidentIndividualDonation() {
+		FinPerson fPerson = PersonUtil.getBachelorMale();
+		fPerson.setResidentStatus(FiConstants.RESIDENT_RESIDENT);
+		fPerson.setAssesseeType(FiConstants.ASSESSEE_INDIVIDUAL);
+		PersonUtil.addDonation(fPerson, 20000, "Prime Minister’s National Relief Fund", "Donation to Prime Minister’s National Relief Fund");
+		FinPersonResult finResult = dService.calculateBenefits(fPerson);
+
+		// Verify section and amount deducted
+
+		boolean result = PersonUtil.hasSectionWithAmount(finResult.getDeductions(), "80G", 20000);
+
+		assertTrue(result);
+	}	
 	
 	
+	@Test
+	public void test80GResidentIndividualDonationTwoTimesSameScheme() {
+		FinPerson fPerson = PersonUtil.getBachelorMale();
+		fPerson.setResidentStatus(FiConstants.RESIDENT_RESIDENT);
+		fPerson.setAssesseeType(FiConstants.ASSESSEE_INDIVIDUAL);
+		PersonUtil.addDonation(fPerson, 20000, "Prime Minister’s National Relief Fund", "Donation to Prime Minister’s National Relief Fund");
+		PersonUtil.addDonation(fPerson, 20000, "Prime Minister’s National Relief Fund", "Donation to Prime Minister’s National Relief Fund again");
+		FinPersonResult finResult = dService.calculateBenefits(fPerson);
+
+		
+		boolean totalResult = PersonUtil.hasSectionWithAmount(finResult.getPlannedDeductions(), "80G", 40000);
+
+		assertTrue(totalResult);
+	}		
+	
+	@Test
+	public void test80GResidentIndividualDonationTwoDifferentScheme() {
+		FinPerson fPerson = PersonUtil.getBachelorMale();
+		fPerson.setResidentStatus(FiConstants.RESIDENT_RESIDENT);
+		fPerson.setAssesseeType(FiConstants.ASSESSEE_INDIVIDUAL);
+		PersonUtil.addDonation(fPerson, 20000, "Prime Minister’s National Relief Fund", "Donation to Prime Minister’s National Relief Fund");
+		PersonUtil.addDonation(fPerson, 20000, "National Sports Fund", "Donation to National Sports Fund");
+		FinPersonResult finResult = dService.calculateBenefits(fPerson);
+
+		
+		boolean totalResult = PersonUtil.hasSectionWithAmount(finResult.getPlannedDeductions(), "80G", 40000);
+
+		assertTrue(totalResult);
+	}		
 	
 }
