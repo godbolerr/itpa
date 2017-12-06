8f0004394fb08f59a83fba110afe7ca7aa66ba9a
115750 <115750@192.168.0.5>
2017-12-06 11:47:24 +0530

    80D Partially tested

---
+++ src/test/java/com/work/itpa/service/Rule80D2Test.java
@@ -0,0 +1,75 @@
+package com.work.itpa.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.work.itpa.domain.Expense;
+import com.work.itpa.domain.FiConstants;
+import com.work.itpa.domain.FinPerson;
+import com.work.itpa.domain.FinPersonResult;
+import com.work.itpa.itparules.ItpaApp;
+import com.work.itpa.utils.PersonUtil;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ItpaApp.class)
+public class Rule80D2Test {
+	
+	
+	@Rule public TestName testName = new TestName();
+
+	@Autowired
+	ItpaService dService;
+
+	String sectionName80d = "80D";
+
+
+	@Test
+	public void test80DHealthCheckup() {
+		FinPerson fPerson = PersonUtil.getBachelorMale();
+		fPerson.setResidentialStatus(FiConstants.RESIDENT_RESIDENT);
+		fPerson.setAssesseeType(FiConstants.ASSESSEE_INDIVIDUAL);
+		
+		BigDecimal healthCheckupExpenses = new BigDecimal("75000");
+		
+		BigDecimal expectedAmount80d = new BigDecimal("75000");
+		
+		fPerson.addExpense(new Expense(healthCheckupExpenses,"PRV_HEALTH_CHK","Prventive Health Checkup "));
+		
+		FinPersonResult finResult = dService.calculateBenefits(fPerson);
+
+		boolean result = PersonUtil.hasSection(finResult.getDeductions(), sectionName80d);
+
+		assertTrue(result);
+
+		result = PersonUtil.hasSectionNTimes(finResult.getDeductions(), sectionName80d, 1);
+
+		assertTrue(result);
+
+		result = PersonUtil.hasSectionWithAmount(finResult.getDeductions(), sectionName80d, healthCheckupExpenses);
+
+		assertTrue(result);
+				
+		result = PersonUtil.hasSummarySectionWithAmount(finResult.getSummaryDeductions(), sectionName80d, expectedAmount80d);
+
+		assertEquals(true, result);
+		
+		
+		PersonUtil.logTestResult(testName.getMethodName(), fPerson, finResult);
+		
+
+	}	
+	
+	
+	
+	
+}
