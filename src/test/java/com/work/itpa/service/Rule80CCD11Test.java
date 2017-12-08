67ec7a5e6b1d94d575398f1f0c9979c8bb69ef72
Ravi Godbole <godbolerr@gmail.com>
2017-12-08 10:17:31 +0530

    80CCD1 Tested

--- src/test/java/com/work/itpa/service/Rule80CCD11Test.java
+++ src/test/java/com/work/itpa/service/Rule80CCD11Test.java
@@ -1,5 +1,6 @@
 package com.work.itpa.service;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.math.BigDecimal;
@@ -79,8 +80,71 @@ public class Rule80CCD11Test {
 		result = PersonUtil.hasSectionWithAmount(finResult.getDeductions(), sectionName80ccd11,pensionSchemeContribution);
 
 		assertTrue(result);
+		
+		
+		result = PersonUtil.hasSummarySectionWithAmount(finResult.getSummaryDeductions(), sectionName80ccd11,
+				pensionSchemeContribution);
+
+		assertEquals(true, result);		
 
 		PersonUtil.logTestResult(testName.getMethodName(), fPerson, finResult);
 
 	}
+	
+	
+
+	@Test
+	public void test80CCD1PensionSchemeInvestmentEmployeeMaxLimit() {
+
+		FinPerson fPerson = PersonUtil.getBachelorMale();
+		
+		Person self = PersonUtil.getPersonWithRelation(fPerson, FiConstants.RELATIONSHIP_SELF);
+		self.setAge(20);
+		
+		SystemFlag sflag = new SystemFlag();
+		
+		sflag.setHasSalary(Boolean.TRUE);
+		
+		fPerson.setSystemFlag(sflag);
+
+		BigDecimal basicIncome = new BigDecimal("40000");
+		BigDecimal daIncome = new BigDecimal("10000");
+		
+		BigDecimal pensionSchemeContribution = new BigDecimal("6000");
+		
+		BigDecimal expectedDeduction = new BigDecimal("5000");
+		
+
+		fPerson.addIncome(new Income(basicIncome,Income.Type.BASIC_SALARY,Income.Source.NA,"Basic Income"));
+		
+		fPerson.addIncome(new Income(daIncome,Income.Type.DEARNESS_ALLOWANCE,Income.Source.NA,"Dearness Income"));
+		
+		fPerson.addInvestment(new Investment(pensionSchemeContribution, "PS_EMPLOYEE", "Investment in Pension Scheme by Employee more than Max"));
+		
+		
+		
+		
+		FinPersonResult finResult = dService.calculateBenefits(fPerson);
+
+		boolean result = PersonUtil.hasSection(finResult.getDeductions(), sectionName80ccd11);
+
+		assertTrue(result);
+
+		result = PersonUtil.hasSectionNTimes(finResult.getDeductions(), sectionName80ccd11, 1);
+
+		assertTrue(result);
+
+		result = PersonUtil.hasSectionWithAmount(finResult.getDeductions(), sectionName80ccd11,pensionSchemeContribution);
+
+		assertTrue(result);
+		
+		result = PersonUtil.hasSummarySectionWithAmount(finResult.getSummaryDeductions(), sectionName80ccd11,
+				expectedDeduction);
+
+		assertEquals(true, result);
+		
+
+		PersonUtil.logTestResult(testName.getMethodName(), fPerson, finResult);
+
+	}	
 }
